7407721e61b06e566ae47eb26c959ea5a98c2a72
diegocadilam <diego.rodrigues@cadilam.com.br>
2022-06-29 23:17:47 -0300

    Parte 4 da aula, complementando Merc. Livre

---
+++ parte4/calculadora/calculadora.js
@@ -0,0 +1,24 @@
+function configurarBotoes(){
+    let btnSubtrair = document.querySelector("#btnSubtrair");
+    console.log(btnSubtrair);
+
+    btnSubtrair.addEventListener('click', function(){
+        subtrair();
+    })
+}
+
+function somar(){
+
+}
+
+function subtrair(){
+    alert('Opa, clicou em subtrair');
+}
+
+function multiplicar(){
+    
+}
+
+function dividir(){
+    
+}
